e965e21e285bb4240e1b18229ab058dc93495798
OrpanAp <purificationalex90@gmail.com>
2026-02-04 03:44:56 -0500

    models for laptop, desktop and mobile

--- inventory/admin.py
+++ inventory/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from . import models
 
-# Register your models here.
+
+
+admin.site.register(models.Laptop)
+admin.site.register(models.Desktop)
+admin.site.register(models.Mobile)
--- inventory/models.py
+++ inventory/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
-# Create your models here.
+
+class Device(models.Model):
+    STATUS_CHOICES = (
+        ('AVAILABLE', 'Available for purchase'),
+        ('SOLD', 'Sold'),
+        ('RESTOCKING', 'Restocking soon'),
+    )
+
+    type = models.CharField(max_length=100, blank=False)
+    price = models.IntegerField(default=0)
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES, default="SOLD")
+    issues = models.CharField(max_length=20, default="None")
+
+
+    class Meta:
+        abstract = True
+
+    def __str__(self):
+        return f"Type: { self.type }, Price: { self.price }"
+    
+
+class Laptop(Device):
+    pass
+class Desktop(Device):
+    pass
+class Mobile(Device):
+    pass
